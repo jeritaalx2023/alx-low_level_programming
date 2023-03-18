66b6e3022f2f42fbe060c93f2905eed192133c24
jerita <jchibhabha1@gmail.com>
2023-03-17 20:05:27 -0700

    negative positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,4 @@
-#includ <stdlib.h>
+#include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 /* more headers goes there */
@@ -20,7 +20,7 @@ int main(void)
 	}
 	else if (n < 0)
 	{
-	        printf("%d the number is negative \n", n);
+	  	printf("%d the number is negative \n", n);
 	}
 	else
 	{
