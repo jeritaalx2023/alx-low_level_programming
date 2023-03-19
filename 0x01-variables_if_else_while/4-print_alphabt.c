83486bf39d7c244924aace4301d99bfcb3c1f3d9
jerita <jchibhabha1@gmail.com>
2023-03-18 23:32:48 -0700

    ommitting some parts on stdio

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		if ((c == 'q' || c == 'e') != 1)
+		{
+			putchar(c);
+		}
+
+	}
+	putchar('\n');
+	return (0);
+}
+
