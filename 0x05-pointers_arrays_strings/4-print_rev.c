b9419cc014be483002caba183bdd2fc957f4a636
jerita <jchibhabha1@gmail.com>
2023-03-27 05:25:44 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - This function prints a reverse string
+ *
+ * @s: This is the input string
+ */
+void print_rev(char *s)
+{
+	int count = 0;
+
+	while (*s)
+	{
+		s++;
+		count++;
+	}
+
+	while (count)
+	{
+		 s--;
+		 _putchar(*s);
+		 count--;
+	}
+	 _putchar('\n');
+}
+
