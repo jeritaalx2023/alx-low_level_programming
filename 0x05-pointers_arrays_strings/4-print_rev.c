9271bb39e44117b34f563b14a929f1880760bee7
jerita <jchibhabha1@gmail.com>
2023-03-27 05:54:55 -0700

    reverse gear

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,20 +7,21 @@
  */
 void print_rev(char *s)
 {
+
 	int count = 0;
 
-	while (*s)
+	while	(*s)
 	{
 		s++;
 		count++;
 	}
 
-	while (count)
-	{
-		 s--;
-		 _putchar(*s);
-		 count--;
-	}
-	 _putchar('\n');
+		while	(count)
+		{
+			s--;
+			_putchar(*s);
+			count--;
+		}
+		_putchar('\n');
 }
 
