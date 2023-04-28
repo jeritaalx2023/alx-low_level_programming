b62b0e27353f9745862761519ad19918fee7e703
jerita <jchibhabha1@gmail.com>
2023-04-27 17:34:35 -0700

    to print firstbefore main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * print_intro - prints output  before the main
+ * function is executed
+ */
+
+
+void print_intro(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
