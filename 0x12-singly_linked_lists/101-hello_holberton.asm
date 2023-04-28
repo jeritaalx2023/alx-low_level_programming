bcf2c8719c26ac2ab8f4b0313f5b30bf29b88316
jerita <jchibhabha1@gmail.com>
2023-04-27 18:53:23 -0700

    first assembly code

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -2,15 +2,16 @@ section .text
 	extern    printf
 	global main
 main:
-
-	mov ecx, msg
+	mov esi, msg
+	mov edi, fmt
 	mov ebx, 1
-	mov eax, 4
+	mov eax, 0
 	call  printf
 	int 0*80
 
-	mov eax, 1
+	mov eax, 0
 	int 0*080
+	ret
 
 section .data
 	msg db "Hello, Holberton",0
