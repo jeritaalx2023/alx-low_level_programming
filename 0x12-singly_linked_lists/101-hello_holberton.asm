dbbbf93b1d70b1549db46be33f6754d154660e83
jerita <jchibhabha1@gmail.com>
2023-04-27 18:13:48 -0700

    first assembly code

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,19 @@
+section .text 
+	global  _start
+	extern    printf
+
+
+_start:
+	mov edx, len
+	mov ecx, msg
+	mov ebx, 1
+	mov eax, 4
+	call  printf
+	int 0*80
+
+	mov eax, 1
+	int 0*080
+
+section .data
+	msg db "Hello, Holberton",0*a
+	len equ $ -msg
