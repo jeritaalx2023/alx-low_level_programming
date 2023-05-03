8654946bbf6033e82e821abf3a7f0733fb6abf48
jerita <jchibhabha1@gmail.com>
2023-05-02 19:37:20 -0700

    count the elements in a linked list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * listint_len - function returns the count  of elements in a linked lists
+ * @h: linked list to traverse
+ *
+ * Return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (num);
+}
+
+
